b2e3c1c5d743c782eab72f6e187b9158e63341b4
Christopher Koster <christopher.koster@palringo.com>
2020-12-10 21:50:57 -0600

    Fixed OnReconnect Handling

--- Handlers/HandleReconnected.js
+++ Handlers/HandleReconnected.js
@@ -1,4 +1,5 @@
 const Client = require('../Client');
+const { updateValues } = require('../Constants/CacheUtils');
 
 /**
  * 
@@ -6,14 +7,23 @@ const Client = require('../Client');
  */
 module.exports = async client => {
     client.On.Reconnected = async () => {
-        // Subscribe to Messages
         await client.Socket.Request('message group subscribe');
         await client.Socket.Request('message private subscribe');
 
         // Resubscribe to fetched users
-        await client.Socket.RequestUsersById(...client.Users.map(t => t.Id));
+        await client.Socket.RequestUsersById(client.Users.map(t => t.Id));
 
         // Resubscribe to Fetched Groups
-        await client.Socket.RequestGroupsById(...client.Groups.map(t => t.Id));
+        await client.Socket.RequestGroupsById(client.Groups.map(t => t.Id));
+
+        // Resubscribe to Group Member Lists
+        let mlLoaded = client.Groups.filter(t => t.MemberListLoaded).map(t => t.Id);
+
+        for (let i = 0; i < mlLoaded; i++)
+            try {
+                await client.Socket.RequestGroupMemberList(mlLoaded[i]);
+            } catch (err) { 
+                updateValues(client.Groups, 'Id', mlLoaded[i], { MemberList: [], MemberListLoaded: false });
+            }
     }
 }
