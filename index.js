56799131118fade1ff4e81c1d99667818bc3d495
Christopher Koster <christopher.koster@palringo.com>
2020-12-14 08:23:56 -0600

    Updated Imports/Exports & Remove Manager
    Updated the Imports/Exports, removed command manager as moving to separate package

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const Client = require('./Client');
 const Events = require('./Helpers/Events');
+const Group = require('./Models/Group/Group');
+const GroupAudioConfig = require('./Models/Group/GroupAudioConfig');
+const GroupAudioCounts = require('./Models/Group/GroupAudioCounts');
 const GroupManager = require('./Managers/GroupManager');
+const GroupMember = require('./Models/GroupMember/GroupMember');
+const GroupRole = require('./Enums/GroupRole');
+const Message = require('./Models/Message/Message');
 const Socket = require('./Network/Socket');
 const User = require('./Models/User/User');
+const UserExtended = require('./Models/User/UserExtended');
 const UserManager = require('./Managers/UserManager');
 
 module.exports = {
     Client,
     Events,
+    Group,
+    GroupAudioConfig,
+    GroupAudioCounts,
     GroupManager,
+    GroupMember,
+    GroupRole,
+    Message,
     Socket,
+    User,
+    UserExtended,
     UserManager
 }
