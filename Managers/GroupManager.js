ba52b8336596ce274d41279c4ad6209174b0352e
Christopher Koster <christopher.koster@palringo.com>
2021-01-01 20:29:20 -0600

    Group Stats

--- Managers/GroupManager.js
+++ Managers/GroupManager.js
@@ -2,6 +2,12 @@ const { assign } = require('./util');
 const Client = require('../Client');
 const Group = require('../Models/Group/Group');
 const Requests = require('../Network/IO/Requests');
+const StatsDetails = require('../Models/GroupStats/StatsDetails');
+const StatsSubscriber = require('../Models/GroupStats/StatsSubscriber');
+const StatsSubscriberTop = require('../Models/GroupStats/StatsSubscriberTop');
+const StatsSubscriberTopWord = require('../Models/GroupStats/StatsSubscriberTopWord');
+const { StatsTrendsDate, StatsTrendsDay, StatsTrendsHour } = require('../Models/GroupStats/StatsTrends');
+const GroupStats = require('../Models/GroupStats/GroupStats');
 
 module.exports = class GroupManager {
     /**
@@ -53,5 +59,37 @@ module.exports = class GroupManager {
 
             return groups;
         } catch { return []; };
+    }
+
+    /**
+     * Get the stats for a group
+     * @param {number} id the id of the group
+     * @returns {GroupStats}
+     */
+    GetStats = async (id) => {
+        try {
+            let response = await Requests.GroupStats(this.#Client.V3, id);
+
+            let { details, trendsHour, trendsDay, trends, top25, next30, topWord, topQuestion, topEmoticon, topHappy, topSad, topSwear, topImage, topAction } = response.body;
+
+            details = assign(new StatsDetails, details);
+            trendsHour = trendsHour.map(th => assign(new StatsTrendsHour, th));
+            trendsDay = trendsDay.map(td => assign(new StatsTrendsDay, td));
+            trends = trends.map(t => assign(new StatsTrendsDate, t));
+            top25 = top25.map(t25 => assign(new StatsSubscriber, t25));
+            next30 = next30.map(n30 => assign(new StatsSubscriber, n30));
+            topWord = topWord.map(tw => assign(new StatsSubscriberTopWord, tw));
+            topQuestion = topQuestion.map(tq => assign(new StatsSubscriberTop, tq));
+            topEmoticon = topEmoticon.map(te => assign(new StatsSubscriberTop, te));
+            topHappy = topHappy.map(th => assign(new StatsSubscriberTop, th));
+            topSad = topHappy.map(ts => assign(new StatsSubscriberTop, ts));
+            topSwear = topSwear.map(ts => assign(new StatsSubscriberTop, ts));
+            topImage = topImage.map(ti => assign(new StatsSubscriberTop, ti));
+            topAction = topAction.map(ta => assign(new StatsSubscriberTop, ta));
+
+            let stats = assign(new GroupStats, { details, trendsHour, trendsDay, trends, top25, next30, topWord, topQuestion, topEmoticon, topHappy, topSad, topSwear, topImage, topAction });
+
+            return stats;
+        } catch { return null; };
     }
 }
