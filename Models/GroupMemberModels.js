b2d00a91bc2290902483004a38e231e2399cf790
Christopher Koster <christopher.koster@palringo.com>
2021-01-04 10:07:48 -0600

    Added type to model - stash of work

--- Models/GroupMemberModels.js
+++ Models/GroupMemberModels.js
@@ -1,24 +1,40 @@
 class GroupMemberAdditionalInformation {
+    
+    /**
+     * @type {string}
+     */
     Hash;
+    
+    /**
+     * @type {string}
+     */
     NicknameShort;
+    
+    /**
+     * @type {number}
+     */
     OnlineState;
+    
+    /**
+     * @type {number}
+     */
     Privileges;
 }
 
 class GroupMember {
     
     /**
-     * 
+     * @type {number}
      */
     Id;
     
     /**
-     * 
+     * @type {number}
      */
     Capabilities;
     
     /**
-     * 
+     * @type {GroupMemberAdditionalInformation}
      */
     AdditionalInfo = new GroupMemberAdditionalInformation;
 }
